c82fefd2cb28ecd96eb3a811fc3baf92beff2747
katie_hodges <katehodges@me.com>
2020-01-25 21:57:50 -0500

    adding final js, css

--- js/main.js
+++ js/main.js
@@ -2,17 +2,75 @@
     // this is a module pattern, also called (function() {
       const mySpan = document.querySelector('main'),
             theSVG = document.querySelector('.imgColour'),
+            guitarSVG = document.querySelector('.Vguitar'),
+            cassetteSVG = document.querySelector('.Cassette'),
+            soundpadSVG = document.querySelector('.soundPad'),
+            saxSVG = document.querySelector('.saxophone'),            
+            violinSVG = document.querySelector('.Violin'),
+            trumpetSVG = document.querySelector('.Trumpet'),
+            drumSVG = document.querySelector('.drumsPink'),
+            bartSVG = document.querySelector('.simpson'),
+            keytarSVG = document.querySelector('.keyTar');
+
+      console.log ('Dear diary,');
+      
+      let allSVGs = document.querySelectorAll(".svg")
 
       function toggleSelected() {
-        // we want to add some kind of indicator
-        // that someone has selected an icon
-
-        // debugger;
         this.classList.toggle('selected');
       }
 
+      function toggleSelectedGuitar() {
+        this.classList.toggle('guitarSelected');
+      }
+      
+      function toggleSelectedCassette() {
+        this.classList.toggle('cassetteSelected');
+      }
+      
+      function toggleSelectedSoundpad() {
+        this.classList.toggle('soundpadSelected');
+      }
+      
+      function toggleSelectedSaxophone() {
+        this.classList.toggle('saxSelected');
+      }
+
+      function toggleSelectedViolin() {
+        this.classList.toggle('violinSelected');
+      }
+
+      function toggleSelectedTrumpet() {
+        this.classList.toggle('trumpetSelected');
+      }
+
+      function toggleSelectedBart() {
+        this.classList.toggle('bartSelected');
+      }
+
+      function toggleSelectedDrums() {
+        this.classList.toggle('drumSelected');
+      }
+
+      function toggleSelectedKeytar() {
+        this.classList.toggle('keytarSelected');
+      }
+
+      function logSVG() {
+        console.log(this.id);
+      }
 
     // set up our triggers here
     mySpan.addEventListener('click', toggleSelected);
     theSVG.addEventListener('click', toggleSelected);
+    guitarSVG.addEventListener('click', toggleSelectedGuitar);
+    cassetteSVG.addEventListener('click', toggleSelectedCassette);
+    soundpadSVG.addEventListener('click', toggleSelectedSoundpad);
+    saxSVG.addEventListener('click', toggleSelectedSaxophone);
+    violinSVG.addEventListener('click', toggleSelectedViolin);
+    trumpetSVG.addEventListener('click', toggleSelectedTrumpet);
+    drumSVG.addEventListener('click', toggleSelectedDrums);
+    bartSVG.addEventListener('click', toggleSelectedBart);
+    keytarSVG.addEventListener('click', toggleSelectedKeytar);
+    allSVGs.forEach(item => item.addEventListener("click", logSVG));
 })();
